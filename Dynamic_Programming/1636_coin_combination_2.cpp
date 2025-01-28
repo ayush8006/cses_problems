7bc35cdb6c86d9ea827ae8efa4ade1464798841a
ayush8006 <ayushagrawal8006@gmail.com>
2025-01-28 22:03:11 +0530

    coin combination 2

---
+++ Dynamic_Programming/1636_coin_combination_2.cpp
@@ -0,0 +1,37 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+const int MOD = 1e9+7; 
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, x;
+    cin >> n >> x;
+
+    vector<int> coins(n);
+    for (int i = 0; i < n; i++) {
+        cin >> coins[i];
+    }
+
+    // DP array to store the minimum coins needed for each sum
+    vector<int> dp(x + 1, 0);
+    dp[0] = 1; // Base case: 0 coins needed to form sum 0
+    for(int j=0;j<n;j++){
+        for (int i = 1; i <= x; i++) {
+       
+            if (i - coins[j] >= 0) {
+                dp[i] =(dp[i]+dp[i - coins[j]])%MOD ;
+            }
+        
+          }
+    }
+
+    // If dp[x] is still INF, it's not possible to form the sum x
+    cout << (dp[x] %MOD) << "\n";
+
+    return 0;
+}
